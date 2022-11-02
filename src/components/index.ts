5868160923d2a37d4346f9cf3896fa5a9aca2e0a
Yash Johri <yashjohri1200@gmail.com>
2022-11-02 17:56:06 +0530

    [Icon] Remove copies

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,39 +1,39 @@
-import ExpandMoreIcon from './ExpandMoreIcon';
-import ArrowDownIcon from './ArrowDownIcon';
-import ArrrowUpIcon from './ArrrowUpIcon';
-import ArrowForwardIcon from './ArrowForwardIcon';
-import CheckCircleIcon from './CheckCircleIcon';
-import CheckIcon from './CheckIcon';
-import ChevronLeftIcon from './ChevronLeftIcon';
-import ChevronRightIcon from './ChevronRightIcon';
-import ClearIcon from './ClearIcon';
-import CloseIcon from './CloseIcon';
-import ContactSupportIcon from './ContactSupportIcon';
-import ContentCopyIcon from './ContentCopyIcon';
-import KeyBoardArrowDownIcon from './KeyBoardArrowDownIcon';
-import KeyBoardArrowUpIcon from './KeyBoardArrowUpIcon';
-import KeyBoardLeftIcon from './KeyBoardLeftIcon';
-import StarRateIcon from './StarRateIcon';
-import MenuIcon from './MenuIcon';
-import KeyBoardRightIcon from './KeyBoardRightIcon';
+import ArrowDownIcon from './ArrowDownIcon'
+import ArrowForwardIcon from './ArrowForwardIcon'
+import ArrrowUpIcon from './ArrrowUpIcon'
+import CheckCircleIcon from './CheckCircleIcon'
+import CheckIcon from './CheckIcon'
+import ChevronLeftIcon from './ChevronLeftIcon'
+import ChevronRightIcon from './ChevronRightIcon'
+import ClearIcon from './ClearIcon'
+import CloseIcon from './CloseIcon'
+import ContactSupportIcon from './ContactSupportIcon'
+import ContentCopyIcon from './ContentCopyIcon'
+import ExpandMoreIcon from './ExpandMoreIcon'
+import KeyBoardArrowDownIcon from './KeyBoardArrowDownIcon'
+import KeyBoardArrowUpIcon from './KeyBoardArrowUpIcon'
+import KeyBoardLeftIcon from './KeyBoardLeftIcon'
+import KeyBoardRightIcon from './KeyBoardRightIcon'
+import MenuIcon from './MenuIcon'
+import StarRateIcon from './StarRateIcon'
 
 export {
-  ExpandMoreIcon,
-  ArrowDownIcon,
-  ArrowForwardIcon,
-  ArrrowUpIcon,
-  CheckCircleIcon,
-  CheckIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  ClearIcon,
-  CloseIcon,
-  ContactSupportIcon,
-  ContentCopyIcon,
-  KeyBoardArrowDownIcon,
-  KeyBoardArrowUpIcon,
-  KeyBoardLeftIcon,
-  KeyBoardRightIcon,
-  MenuIcon,
-  StarRateIcon,
-};
+	ArrowDownIcon,
+	ArrowForwardIcon,
+	ArrrowUpIcon,
+	CheckCircleIcon,
+	CheckIcon,
+	ChevronLeftIcon,
+	ChevronRightIcon,
+	ClearIcon,
+	CloseIcon,
+	ContactSupportIcon,
+	ContentCopyIcon,
+	ExpandMoreIcon,
+	KeyBoardArrowDownIcon,
+	KeyBoardArrowUpIcon,
+	KeyBoardLeftIcon,
+	KeyBoardRightIcon,
+	MenuIcon,
+	StarRateIcon,
+}
