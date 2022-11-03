59dc99ebf3855bae4d62226b0c361cc7e8574833
frannkenstein <deathakanote@gmail.com>
2022-11-03 12:29:36 +0530

    Rollup re introduced

---
+++ src/Icons/index.ts
@@ -0,0 +1,39 @@
+import ArrowDownIcon from './ArrowDownIcon';
+import ArrowForwardIcon from './ArrowForwardIcon';
+import ArrrowUpIcon from './ArrrowUpIcon';
+import CheckCircleIcon from './CheckCircleIcon';
+import CheckIcon from './CheckIcon';
+import ChevronLeftIcon from './ChevronLeftIcon';
+import ChevronRightIcon from './ChevronRightIcon';
+import ClearIcon from './ClearIcon';
+import CloseIcon from './CloseIcon';
+import ContactSupportIcon from './ContactSupportIcon';
+import ContentCopyIcon from './ContentCopyIcon';
+import ExpandMoreIcon from './ExpandMoreIcon';
+import KeyBoardArrowDownIcon from './KeyBoardArrowDownIcon';
+import KeyBoardArrowUpIcon from './KeyBoardArrowUpIcon';
+import KeyBoardLeftIcon from './KeyBoardLeftIcon';
+import KeyBoardRightIcon from './KeyBoardRightIcon';
+import MenuIcon from './MenuIcon';
+import StarRateIcon from './StarRateIcon';
+
+export {
+  ArrowDownIcon,
+  ArrowForwardIcon,
+  ArrrowUpIcon,
+  CheckCircleIcon,
+  CheckIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ClearIcon,
+  CloseIcon,
+  ContactSupportIcon,
+  ContentCopyIcon,
+  ExpandMoreIcon,
+  KeyBoardArrowDownIcon,
+  KeyBoardArrowUpIcon,
+  KeyBoardLeftIcon,
+  KeyBoardRightIcon,
+  MenuIcon,
+  StarRateIcon,
+};
