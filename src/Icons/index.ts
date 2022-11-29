9d00a2219f0475898b8030fa29a96e99245e3b5f
Yash Johri <yashjohri1200@gmail.com>
2022-11-29 12:21:26 +0530

    [Icons] Some changes

--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -1,43 +1,43 @@
-import ArrowBackIcon from './ArrowBackIcon';
-import ArrowDownIcon from './ArrowDownIcon';
-import ArrowIcon from './Reskin/ArrowIcon';
-import ArrowForwardIcon from './ArrowForwardIcon';
-import ArrowUpIcon from './ArrowUpIcon';
-import CheckCircleIcon from './CheckCircleIcon';
-import CheckIcon from './CheckIcon';
-import ClearIcon from './ClearIcon';
-import ClockIcon from './ClockIcon';
-import ContactSupportIcon from './ContactSupportIcon';
-import ContentCopyIcon from './ContentCopyIcon';
-import ExpandMoreIcon from './ExpandMoreIcon';
-import InfoIcon from './InfoIcon';
-import KeyBoardArrowDownIcon from './KeyBoardArrowDownIcon';
-import KeyBoardArrowLeftIcon from './KeyBoardArrowLeftIcon';
-import KeyBoardArrowRightIcon from './KeyBoardArrowRightIcon';
-import KeyBoardArrowUpIcon from './KeyBoardArrowUpIcon';
-import MenuIcon from './MenuIcon';
-import ProcessIcon from './ProcessIcon';
-import StarRateIcon from './StarRateIcon';
+import ArrowBackIcon from './ArrowBackIcon'
+import ArrowDownIcon from './ArrowDownIcon'
+import ArrowForwardIcon from './ArrowForwardIcon'
+import ArrowIcon from './ArrowIcon'
+import ArrowUpIcon from './ArrowUpIcon'
+import CheckCircleIcon from './CheckCircleIcon'
+import CheckIcon from './CheckIcon'
+import ClearIcon from './ClearIcon'
+import ClockIcon from './ClockIcon'
+import ContactSupportIcon from './ContactSupportIcon'
+import ContentCopyIcon from './ContentCopyIcon'
+import ExpandMoreIcon from './ExpandMoreIcon'
+import InfoIcon from './InfoIcon'
+import KeyBoardArrowDownIcon from './KeyBoardArrowDownIcon'
+import KeyBoardArrowLeftIcon from './KeyBoardArrowLeftIcon'
+import KeyBoardArrowRightIcon from './KeyBoardArrowRightIcon'
+import KeyBoardArrowUpIcon from './KeyBoardArrowUpIcon'
+import MenuIcon from './MenuIcon'
+import ProcessIcon from './ProcessIcon'
+import StarRateIcon from './StarRateIcon'
 
 export {
-  ArrowIcon,
-  ArrowBackIcon,
-  ArrowDownIcon,
-  ArrowForwardIcon,
-  ArrowUpIcon,
-  CheckCircleIcon,
-  CheckIcon,
-  ClearIcon,
-  ClockIcon,
-  ContactSupportIcon,
-  ContentCopyIcon,
-  ExpandMoreIcon,
-  InfoIcon,
-  KeyBoardArrowDownIcon,
-  KeyBoardArrowLeftIcon,
-  KeyBoardArrowRightIcon,
-  KeyBoardArrowUpIcon,
-  MenuIcon,
-  ProcessIcon,
-  StarRateIcon,
-};
+	ArrowBackIcon,
+	ArrowDownIcon,
+	ArrowForwardIcon,
+	ArrowIcon,
+	ArrowUpIcon,
+	CheckCircleIcon,
+	CheckIcon,
+	ClearIcon,
+	ClockIcon,
+	ContactSupportIcon,
+	ContentCopyIcon,
+	ExpandMoreIcon,
+	InfoIcon,
+	KeyBoardArrowDownIcon,
+	KeyBoardArrowLeftIcon,
+	KeyBoardArrowRightIcon,
+	KeyBoardArrowUpIcon,
+	MenuIcon,
+	ProcessIcon,
+	StarRateIcon,
+}
