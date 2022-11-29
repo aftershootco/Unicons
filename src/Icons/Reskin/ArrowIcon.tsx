5c073c0ab2e7b173a113b0d15b3dc22816a24e03
frannkenstein <deathakanote@gmail.com>
2022-11-29 10:03:14 +0530

    Some new icons

---
+++ src/Icons/Reskin/ArrowIcon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ArrowIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
+  return (
+    <svg
+      width='6'
+      height='12'
+      viewBox='0 0 6 12'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path d='M0.25 11.5L5.75 6L0.25 0.5L0.25 11.5Z' fill='#777777' />
+    </svg>
+  );
+};
+
+export default React.memo(ArrowIcon);
--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -1,35 +1,43 @@
-import ArrowBackIcon from './ArrowBackIcon'
-import ArrowDownIcon from './ArrowDownIcon'
-import ArrowForwardIcon from './ArrowForwardIcon'
-import ArrowUpIcon from './ArrowUpIcon'
-import CheckCircleIcon from './CheckCircleIcon'
-import CheckIcon from './CheckIcon'
-import ClearIcon from './ClearIcon'
-import ContactSupportIcon from './ContactSupportIcon'
-import ContentCopyIcon from './ContentCopyIcon'
-import ExpandMoreIcon from './ExpandMoreIcon'
-import KeyBoardArrowDownIcon from './KeyBoardArrowDownIcon'
-import KeyBoardArrowLeftIcon from './KeyBoardArrowLeftIcon'
-import KeyBoardArrowRightIcon from './KeyBoardArrowRightIcon'
-import KeyBoardArrowUpIcon from './KeyBoardArrowUpIcon'
-import MenuIcon from './MenuIcon'
-import StarRateIcon from './StarRateIcon'
+import ArrowBackIcon from './ArrowBackIcon';
+import ArrowDownIcon from './ArrowDownIcon';
+import ArrowIcon from './Reskin/ArrowIcon';
+import ArrowForwardIcon from './ArrowForwardIcon';
+import ArrowUpIcon from './ArrowUpIcon';
+import CheckCircleIcon from './CheckCircleIcon';
+import CheckIcon from './CheckIcon';
+import ClearIcon from './ClearIcon';
+import ClockIcon from './ClockIcon';
+import ContactSupportIcon from './ContactSupportIcon';
+import ContentCopyIcon from './ContentCopyIcon';
+import ExpandMoreIcon from './ExpandMoreIcon';
+import InfoIcon from './InfoIcon';
+import KeyBoardArrowDownIcon from './KeyBoardArrowDownIcon';
+import KeyBoardArrowLeftIcon from './KeyBoardArrowLeftIcon';
+import KeyBoardArrowRightIcon from './KeyBoardArrowRightIcon';
+import KeyBoardArrowUpIcon from './KeyBoardArrowUpIcon';
+import MenuIcon from './MenuIcon';
+import ProcessIcon from './ProcessIcon';
+import StarRateIcon from './StarRateIcon';
 
 export {
-	ArrowBackIcon,
-	ArrowDownIcon,
-	ArrowForwardIcon,
-	ArrowUpIcon,
-	CheckCircleIcon,
-	CheckIcon,
-	ClearIcon,
-	ContactSupportIcon,
-	ContentCopyIcon,
-	ExpandMoreIcon,
-	KeyBoardArrowDownIcon,
-	KeyBoardArrowLeftIcon,
-	KeyBoardArrowRightIcon,
-	KeyBoardArrowUpIcon,
-	MenuIcon,
-	StarRateIcon,
-}
+  ArrowIcon,
+  ArrowBackIcon,
+  ArrowDownIcon,
+  ArrowForwardIcon,
+  ArrowUpIcon,
+  CheckCircleIcon,
+  CheckIcon,
+  ClearIcon,
+  ClockIcon,
+  ContactSupportIcon,
+  ContentCopyIcon,
+  ExpandMoreIcon,
+  InfoIcon,
+  KeyBoardArrowDownIcon,
+  KeyBoardArrowLeftIcon,
+  KeyBoardArrowRightIcon,
+  KeyBoardArrowUpIcon,
+  MenuIcon,
+  ProcessIcon,
+  StarRateIcon,
+};
