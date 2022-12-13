d26dfc42ecf52b0f1d50395517c2e06fb2da97d6
manis <manishdoley23@gmail.com>
2022-12-13 14:02:54 +0530

    [SortPhotos] added new icons for sort photos

--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -1,59 +1,63 @@
-import ArrowBackIcon from './ArrowBackIcon'
-import ArrowDownIcon from './ArrowDownIcon'
-import ArrowForwardIcon from './ArrowForwardIcon'
-import ArrowIcon from './ArrowIcon'
-import ArrowUpIcon from './ArrowUpIcon'
-import CheckCircleIcon from './CheckCircleIcon'
-import CheckIcon from './CheckIcon'
-import ClearIcon from './ClearIcon'
-import ClockIcon from './ClockIcon'
-import ContactSupportIcon from './ContactSupportIcon'
-import ContentCopyIcon from './ContentCopyIcon'
-import ExpandMoreIcon from './ExpandMoreIcon'
-import InfoIcon from './InfoIcon'
-import KeyBoardArrowDownIcon from './KeyBoardArrowDownIcon'
-import KeyBoardArrowLeftIcon from './KeyBoardArrowLeftIcon'
-import KeyBoardArrowRightIcon from './KeyBoardArrowRightIcon'
-import KeyBoardArrowUpIcon from './KeyBoardArrowUpIcon'
-import MenuIcon from './MenuIcon'
-import ProcessIcon from './ProcessIcon'
-import AfterShootLogo from './Reskin/AfterShootLogo'
-import DeleteIcon from './Reskin/DeleteIcon'
-import ErrorIcon from './Reskin/ErrorIcon'
-import ReloadIcon from './Reskin/ReloadIcon'
-import AlertIcon from './Reskin/AlertIcon'
-import BorderColorIcon from './Reskin/BorderColorIcon'
-import CartIcon from './Reskin/CartIcon'
-import DiamondIcon from './Reskin/DiamonIcon'
-import DropDownIcon from './Reskin/DropDownIcon'
-import EditIcon from './Reskin/EditIcon'
-import EditPhotoIcon from './Reskin/EditPhotoIcon'
-import FriendsIcon from './Reskin/FriendsIcon'
-import ImageNotFound from './Reskin/ImageNotFound'
-import InAcitveHelp from './Reskin/InAcitveHelp'
-import InActiveColorIcon from './Reskin/InActiveColorIcon'
-import InActiveFilter from './Reskin/InActiveFilter'
-import InActiveGridIcon from './Reskin/InActiveGridIcon'
-import InactiveLoupeIcon from './Reskin/InactiveLoupeIcon'
-import InActiveShortCutIcon from './Reskin/InActiveShortCutIcon'
-import InActiveSortIcon from './Reskin/InActiveSortIcon'
-import InActiveSprayCanIcon from './Reskin/InActiveSprayCanIcon'
-import InActiveStarIcon from './Reskin/InActiveStarIcon'
-import InfoIconTransparent from './Reskin/InfoIconTransparent'
-import LeftArrow from './Reskin/LeftArrow'
-import MouseLeftSelected from './Reskin/MouseLeftSelected'
-import MouseRightSelected from './Reskin/MouseRightSelected'
-import ShareIcon from './Reskin/ShareIcon'
-import UploadFolder from './Reskin/UploadFolder'
-import ZoomIcon from './Reskin/ZoomIcon'
-import ExitApp from './Reskin/ExitApp'
-import StarRateIcon from './StarRateIcon'
+import ArrowBackIcon from './ArrowBackIcon';
+import ArrowDownIcon from './ArrowDownIcon';
+import ArrowForwardIcon from './ArrowForwardIcon';
+import ArrowIcon from './ArrowIcon';
+import ArrowUpIcon from './ArrowUpIcon';
+import CheckCircleIcon from './CheckCircleIcon';
+import CheckIcon from './CheckIcon';
+import ClearIcon from './ClearIcon';
+import ClockIcon from './ClockIcon';
+import ContactSupportIcon from './ContactSupportIcon';
+import ContentCopyIcon from './ContentCopyIcon';
+import ExpandMoreIcon from './ExpandMoreIcon';
+import InfoIcon from './InfoIcon';
+import KeyBoardArrowDownIcon from './KeyBoardArrowDownIcon';
+import KeyBoardArrowLeftIcon from './KeyBoardArrowLeftIcon';
+import KeyBoardArrowRightIcon from './KeyBoardArrowRightIcon';
+import KeyBoardArrowUpIcon from './KeyBoardArrowUpIcon';
+import MenuIcon from './MenuIcon';
+import ProcessIcon from './ProcessIcon';
+import AlertIcon from './Reskin/AlertIcon';
+import BorderColorIcon from './Reskin/BorderColorIcon';
+import CartIcon from './Reskin/CartIcon';
+import DescAlphaNotSelectedIcon from './Reskin/DescAlphaNotSelectedIcon';
+import DescAlphaSelectedIcon from './Reskin/DescAlphaSelectedIcon';
+import DescNumNotSelectedIcon from './Reskin/DescNumNotSelectedIcon';
+import DescNumSelectedIcon from './Reskin/DescNumSelectedIcon';
+import DiamondIcon from './Reskin/DiamonIcon';
+import DropDownIcon from './Reskin/DropDownIcon';
+import EditIcon from './Reskin/EditIcon';
+import EditPhotoIcon from './Reskin/EditPhotoIcon';
+import FriendsIcon from './Reskin/FriendsIcon';
+import InAcitveHelp from './Reskin/InAcitveHelp';
+import InActiveColorIcon from './Reskin/InActiveColorIcon';
+import InActiveFilter from './Reskin/InActiveFilter';
+import InActiveGridIcon from './Reskin/InActiveGridIcon';
+import InactiveLoupeIcon from './Reskin/InactiveLoupeIcon';
+import InActiveShortCutIcon from './Reskin/InActiveShortCutIcon';
+import InActiveSortIcon from './Reskin/InActiveSortIcon';
+import InActiveSprayCanIcon from './Reskin/InActiveSprayCanIcon';
+import InActiveStarIcon from './Reskin/InActiveStarIcon';
+import IncAlphaNotSelectedIcon from './Reskin/IncAlphaNotSelectedIcon';
+import IncAlphaSelectedIcon from './Reskin/IncAlphaSelectedIcon';
+import IncNumNotSelectedIcon from './Reskin/IncNumNotSelectedIcon';
+import IncNumSelectedIcon from './Reskin/IncNumSelectedIcon';
+import InfoIconTransparent from './Reskin/InfoIconTransparent';
+import MouseLeftSelected from './Reskin/MouseLeftSelected';
+import MouseRightSelected from './Reskin/MouseRightSelected';
+import ShareIcon from './Reskin/ShareIcon';
+import ZoomIcon from './Reskin/ZoomIcon';
+import StarRateIcon from './StarRateIcon';
 
 export {
-	AfterShootLogo,
-	DeleteIcon,
-	ErrorIcon,
-	ReloadIcon,
+	DescAlphaSelectedIcon,
+	DescAlphaNotSelectedIcon,
+	IncAlphaSelectedIcon,
+	IncAlphaNotSelectedIcon,
+	DescNumSelectedIcon,
+	DescNumNotSelectedIcon,
+	IncNumSelectedIcon,
+	IncNumNotSelectedIcon,
 	EditPhotoIcon,
 	DiamondIcon,
 	ShareIcon,
@@ -100,4 +104,5 @@ export {
 	UploadFolder,
 	ExitApp,
 	LeftArrow,
-}
+};
+
