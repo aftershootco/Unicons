14927ff66d7001fe9462d40f2bda0a4ba0f7aede
booi-dev <booimangang@gmail.com>
2024-01-05 11:15:11 +0530

    add: svgs from main app

---
+++ src/svg/windows-titlebar-icons/index.ts
@@ -0,0 +1,6 @@
+import Close from './close.svg'
+import Maximize from './maximize.svg'
+import Minimize from './minimize.svg'
+import Unmaximize from './unmaximize.svg'
+
+export { Close, Maximize, Minimize, Unmaximize }
