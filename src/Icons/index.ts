745fe38ee47475ce61b4cd1c0f59c0870aed63d8
booi-dev <booimangang@gmail.com>
2024-01-06 16:46:56 +0530

    update: restructure folders

--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -1,162 +1,180 @@
-import ArrowBackIcon from './ArrowBackIcon'
-import ArrowDownIcon from './ArrowDownIcon'
-import ArrowForwardIcon from './ArrowForwardIcon'
-import ArrowIcon from './ArrowIcon'
-import ArrowUpIcon from './ArrowUpIcon'
+import ArrowBackIcon from '../components/ArrowBackIcon'
+import ArrowDownIcon from '../components/ArrowDownIcon'
+import ArrowForwardIcon from '../components/ArrowForwardIcon'
+import ArrowIcon from '../components/ArrowIcon'
+import ArrowUpIcon from '../components/ArrowUpIcon'
+
+import AlertIcon from '../Reskin/AlertIcon'
+
+import BorderColorIcon from '../Reskin/BorderColorIcon'
+
+import CartIcon from '../Reskin/CartIcon'
+import CopyIcon from '../Reskin/CopyIcon'
 import CheckCircleIcon from './CheckCircleIcon'
 import CheckIcon from './CheckIcon'
 import ClearIcon from './ClearIcon'
 import ClockIcon from './ClockIcon'
 import ContactSupportIcon from './ContactSupportIcon'
 import ContentCopyIcon from './ContentCopyIcon'
+
+import DeleteIcon from '../Reskin/DeleteIcon'
+import DescAlphaNotSelectedIcon from '../Reskin/DescAlphaNotSelectedIcon'
+import DescAlphaSelectedIcon from '../Reskin/DescAlphaSelectedIcon'
+import DescNumNotSelectedIcon from '../Reskin/DescNumNotSelectedIcon'
+import DescNumSelectedIcon from '../Reskin/DescNumSelectedIcon'
+import DiamondIcon from '../Reskin/DiamonIcon'
+import DropDownIcon from '../Reskin/DropDownIcon'
+
+import EditIcon from '../Reskin/EditIcon'
+import EditPhotoIcon from '../Reskin/EditPhotoIcon'
+import ExitAppIcon from '../Reskin/ExitAppIcon'
+import ExportedIcon from '../Reskin/ExportedIcon'
+import EyeCheckIcon from '../Reskin/EyeCheckIcon'
+import EyeIcon from '../Reskin/EyeIcon'
+import EyeOffIcon from '../Reskin/EyeOffIcon'
 import ExpandMoreIcon from './ExpandMoreIcon'
+
+import FolderIcon from '../Reskin/FolderIcon'
+import FriendsIcon from '../Reskin/FriendsIcon'
+
+import GridNotSelectedIcon from '../Reskin/GridNotSelectedIcon'
+import GridSelectedIcon from '../Reskin/GridSelectedIcon'
+
+import ImageNotFound from '../Reskin/ImageNotFound'
+import InActiveHelp from '../Reskin/InAcitveHelp'
+import InActiveColorIcon from '../Reskin/InActiveColorIcon'
+import InActiveFilter from '../Reskin/InActiveFilter'
+import InActiveGridIcon from '../Reskin/InActiveGridIcon'
+import InActiveShortCutIcon from '../Reskin/InActiveShortCutIcon'
+import InActiveSortIcon from '../Reskin/InActiveSortIcon'
+import InActiveSprayCanIcon from '../Reskin/InActiveSprayCanIcon'
+import InActiveStarIcon from '../Reskin/InActiveStarIcon'
+import InactiveLoupeIcon from '../Reskin/InactiveLoupeIcon'
+import IncAlphaNotSelectedIcon from '../Reskin/IncAlphaNotSelectedIcon'
+import IncAlphaSelectedIcon from '../Reskin/IncAlphaSelectedIcon'
+import IncNumNotSelectedIcon from '../Reskin/IncNumNotSelectedIcon'
+import IncNumSelectedIcon from '../Reskin/IncNumSelectedIcon'
+import InfoIconTransparent from '../Reskin/InfoIconTransparent'
 import InfoIcon from './InfoIcon'
+
 import KeyBoardArrowDownIcon from './KeyBoardArrowDownIcon'
 import KeyBoardArrowLeftIcon from './KeyBoardArrowLeftIcon'
 import KeyBoardArrowRightIcon from './KeyBoardArrowRightIcon'
 import KeyBoardArrowUpIcon from './KeyBoardArrowUpIcon'
+
+import LeftArrow from '../Reskin/LeftArrow'
+import LeftArrowLong from '../Reskin/LeftArrowLong'
+import ListNotSelectedIcon from '../Reskin/ListNotSelectedIcon'
+import ListSelectedIcon from '../Reskin/ListSelectedIcon'
+import LockIcon from '../Reskin/LockIcon'
+import LoopIcon from '../Reskin/LoopIcon'
+
+import MaximizeIcon from '../Reskin/MaximizeIcon'
+import MouseLeftSelected from '../Reskin/MouseLeftSelected'
+import MouseRightSelected from '../Reskin/MouseRightSelected'
 import MenuIcon from './MenuIcon'
+
+import NetworkErrorIcon from '../Reskin/NetworkErrorIcon'
+
 import ProcessIcon from './ProcessIcon'
-import AlertIcon from './Reskin/AlertIcon'
-import BorderColorIcon from './Reskin/BorderColorIcon'
-import CartIcon from './Reskin/CartIcon'
-import CopyIcon from './Reskin/CopyIcon'
-import DeleteIcon from './Reskin/DeleteIcon'
-import DescAlphaNotSelectedIcon from './Reskin/DescAlphaNotSelectedIcon'
-import DescAlphaSelectedIcon from './Reskin/DescAlphaSelectedIcon'
-import DescNumNotSelectedIcon from './Reskin/DescNumNotSelectedIcon'
-import DescNumSelectedIcon from './Reskin/DescNumSelectedIcon'
-import DiamondIcon from './Reskin/DiamonIcon'
-import DropDownIcon from './Reskin/DropDownIcon'
-import EditIcon from './Reskin/EditIcon'
-import EditPhotoIcon from './Reskin/EditPhotoIcon'
-import ExitAppIcon from './Reskin/ExitAppIcon'
-import ExportedIcon from './Reskin/ExportedIcon'
-import EyeCheckIcon from './Reskin/EyeCheckIcon'
-import EyeIcon from './Reskin/EyeIcon'
-import EyeOffIcon from './Reskin/EyeOffIcon'
-import FolderIcon from './Reskin/FolderIcon'
-import FriendsIcon from './Reskin/FriendsIcon'
-import GridNotSelectedIcon from './Reskin/GridNotSelectedIcon'
-import GridSelectedIcon from './Reskin/GridSelectedIcon'
-import ImageNotFound from './Reskin/ImageNotFound'
-import InActiveHelp from './Reskin/InAcitveHelp'
-import InActiveColorIcon from './Reskin/InActiveColorIcon'
-import InActiveFilter from './Reskin/InActiveFilter'
-import InActiveGridIcon from './Reskin/InActiveGridIcon'
-import InactiveLoupeIcon from './Reskin/InactiveLoupeIcon'
-import InActiveShortCutIcon from './Reskin/InActiveShortCutIcon'
-import InActiveSortIcon from './Reskin/InActiveSortIcon'
-import InActiveSprayCanIcon from './Reskin/InActiveSprayCanIcon'
-import InActiveStarIcon from './Reskin/InActiveStarIcon'
-import IncAlphaNotSelectedIcon from './Reskin/IncAlphaNotSelectedIcon'
-import IncAlphaSelectedIcon from './Reskin/IncAlphaSelectedIcon'
-import IncNumNotSelectedIcon from './Reskin/IncNumNotSelectedIcon'
-import IncNumSelectedIcon from './Reskin/IncNumSelectedIcon'
-import InfoIconTransparent from './Reskin/InfoIconTransparent'
-import LeftArrow from './Reskin/LeftArrow'
-import LeftArrowLong from './Reskin/LeftArrowLong'
-import ListNotSelectedIcon from './Reskin/ListNotSelectedIcon'
-import ListSelectedIcon from './Reskin/ListSelectedIcon'
-import LockIcon from './Reskin/LockIcon'
-import LoopIcon from './Reskin/LoopIcon'
-import MaximizeIcon from './Reskin/MaximizeIcon'
-import MouseLeftSelected from './Reskin/MouseLeftSelected'
-import MouseRightSelected from './Reskin/MouseRightSelected'
-import NetworkErrorIcon from './Reskin/NetworkErrorIcon'
-import ReloadIcon from './Reskin/ReloadIcon'
-import RightArrowLong from './Reskin/RightArrowLong'
-import ShareIcon from './Reskin/ShareIcon'
-import SortDuplicatesIcon from './Reskin/SortDuplicatesIcon'
-import SourceNotFoundIcon from './Reskin/SourceNotFound'
-import SprayClickIcon from './Reskin/SprayClickIcon'
-import StorageIcon from './Reskin/StorageIcon'
-import UploadFolder from './Reskin/UploadFolder'
-import WarningIcon from './Reskin/WarningIcon'
-import WarningImgMovedIcon from './Reskin/WarningImgMovedIcon'
-import ZoomIcon from './Reskin/ZoomIcon'
+
+import ReloadIcon from '../Reskin/ReloadIcon'
+import RightArrowLong from '../Reskin/RightArrowLong'
+
+import ShareIcon from '../Reskin/ShareIcon'
+import SortDuplicatesIcon from '../Reskin/SortDuplicatesIcon'
+import SourceNotFoundIcon from '../Reskin/SourceNotFound'
+import SprayClickIcon from '../Reskin/SprayClickIcon'
+import StorageIcon from '../Reskin/StorageIcon'
 import StarRateIcon from './StarRateIcon'
+
 import TickIcon from './TickIcon'
 
+import UploadFolder from '../Reskin/UploadFolder'
+
+import WarningIcon from '../Reskin/WarningIcon'
+import WarningImgMovedIcon from '../Reskin/WarningImgMovedIcon'
+
+import ZoomIcon from '../Reskin/ZoomIcon'
+
 export {
-	EyeOffIcon,
-	EyeIcon,
-	RightArrowLong,
-	LeftArrowLong,
-	StorageIcon,
-	ReloadIcon,
-	LockIcon,
-	SprayClickIcon,
-	SourceNotFoundIcon,
-	SortDuplicatesIcon,
-	LoopIcon,
-	ListSelectedIcon,
-	ListNotSelectedIcon,
-	GridSelectedIcon,
-	GridNotSelectedIcon,
-	MaximizeIcon,
-	EyeCheckIcon,
-	WarningIcon,
-	DeleteIcon,
-	DescAlphaSelectedIcon,
-	DescAlphaNotSelectedIcon,
-	IncAlphaSelectedIcon,
-	IncAlphaNotSelectedIcon,
-	DescNumSelectedIcon,
-	DescNumNotSelectedIcon,
-	IncNumSelectedIcon,
-	IncNumNotSelectedIcon,
-	EditPhotoIcon,
-	DiamondIcon,
-	ShareIcon,
-	CartIcon,
-	FriendsIcon,
+	AlertIcon,
 	ArrowBackIcon,
 	ArrowDownIcon,
 	ArrowForwardIcon,
 	ArrowIcon,
 	ArrowUpIcon,
+	BorderColorIcon,
+	CartIcon,
 	CheckCircleIcon,
 	CheckIcon,
 	ClearIcon,
 	ClockIcon,
 	ContactSupportIcon,
 	ContentCopyIcon,
+	CopyIcon,
+	DeleteIcon,
+	DescAlphaNotSelectedIcon,
+	DescAlphaSelectedIcon,
+	DescNumNotSelectedIcon,
+	DescNumSelectedIcon,
+	DiamondIcon,
+	DropDownIcon,
+	EditIcon,
+	EditPhotoIcon,
+	ExitAppIcon,
 	ExpandMoreIcon,
+	ExportedIcon,
+	EyeCheckIcon,
+	EyeIcon,
+	EyeOffIcon,
+	FolderIcon,
+	FriendsIcon,
+	GridNotSelectedIcon,
+	GridSelectedIcon,
+	ImageNotFound,
+	InActiveColorIcon,
+	InActiveFilter,
+	InActiveGridIcon,
+	InActiveHelp,
+	InActiveShortCutIcon,
+	InActiveSortIcon,
+	InActiveSprayCanIcon,
+	InActiveStarIcon,
+	InactiveLoupeIcon,
+	IncAlphaNotSelectedIcon,
+	IncAlphaSelectedIcon,
+	IncNumNotSelectedIcon,
+	IncNumSelectedIcon,
 	InfoIcon,
+	InfoIconTransparent,
 	KeyBoardArrowDownIcon,
 	KeyBoardArrowLeftIcon,
 	KeyBoardArrowRightIcon,
 	KeyBoardArrowUpIcon,
+	LeftArrow,
+	LeftArrowLong,
+	ListNotSelectedIcon,
+	ListSelectedIcon,
+	LockIcon,
+	LoopIcon,
+	MaximizeIcon,
 	MenuIcon,
-	ProcessIcon,
-	StarRateIcon,
-	ZoomIcon,
 	MouseLeftSelected,
 	MouseRightSelected,
-	EditIcon,
-	AlertIcon,
-	ImageNotFound,
-	InActiveHelp,
-	InActiveColorIcon,
-	InActiveShortCutIcon,
-	InActiveStarIcon,
-	InActiveSprayCanIcon,
-	InActiveSortIcon,
-	InactiveLoupeIcon,
-	InActiveFilter,
-	InActiveGridIcon,
-	InfoIconTransparent,
-	BorderColorIcon,
-	DropDownIcon,
-	UploadFolder,
-	ExitAppIcon,
-	LeftArrow,
-	ExportedIcon,
 	NetworkErrorIcon,
-	CopyIcon,
-	WarningImgMovedIcon,
-	FolderIcon,
+	ProcessIcon,
+	ReloadIcon,
+	RightArrowLong,
+	ShareIcon,
+	SortDuplicatesIcon,
+	SourceNotFoundIcon,
+	SprayClickIcon,
+	StarRateIcon,
+	StorageIcon,
 	TickIcon,
+	UploadFolder,
+	WarningIcon,
+	WarningImgMovedIcon,
+	ZoomIcon,
 }
-
