cbccce5479514af7beafcbb8893f8e6fb444668c
booi-dev <booimangang@gmail.com>
2024-01-09 13:44:06 +0530

    add: spray icon

---
+++ src/Icons/SprayCanIcon.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { IconProps } from '../../types/Icons/types'
+import BaseIcon from '../components/BaseIcon'
+import DEFAULT_ICON from '../constant'
+
+type Props = IconProps & {
+	inActive?: boolean
+}
+
+export const SprayCanIcon = React.forwardRef<SVGSVGElement, Props>((props, forwardedRef) => {
+	// props
+	const { variant = 'primary', color = DEFAULT_ICON.COLOR, size = DEFAULT_ICON.SIZE, inActive = false, ...restProps } = props
+
+	const modifiedColor = inActive ? DEFAULT_ICON.INACTIVE_COLOR : color
+	// variants
+	const primary = (
+		<svg width={size} height={size} viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg' {...restProps} ref={forwardedRef}>
+			<g clip-path='url(#clip0_12973_18863)'>
+				<path
+					d='M5 7.76074V16.667H14.375V7.76074M5 7.76074H14.375M5 7.76074C5 5.94856 6.46907 4.47949 8.28125 4.47949M14.375 7.76074V7.52637C14.375 5.84362 13.0109 4.47949 11.3281 4.47949M8.28125 4.47949V2.60449C8.28125 2.08673 8.70098 1.66699 9.21875 1.66699H10.3906C10.9084 1.66699 11.3281 2.08673 11.3281 2.60449V4.47949M8.28125 4.47949H11.3281'
+					stroke={modifiedColor}
+					stroke-width='1.5'
+					stroke-linecap='round'
+					stroke-linejoin='round'
+				/>
+			</g>
+			<defs>
+				<clipPath id='clip0_12973_18863'>
+					<rect width='20' height='20' fill='white' />
+				</clipPath>
+			</defs>
+		</svg>
+	)
+
+	return <BaseIcon variants={{ primary }} variant={variant} />
+})
+
+export default React.memo(SprayCanIcon)
--- src/Icons/index.ts
+++ src/Icons/index.ts
@@ -1,11 +1,35 @@
+import InActiveStarIcon from '../Reskin/InActiveStarIcon'
+import IncAlphaNotSelectedIcon from '../Reskin/IncAlphaNotSelectedIcon'
+import IncAlphaSelectedIcon from '../Reskin/IncAlphaSelectedIcon'
+import IncNumNotSelectedIcon from '../Reskin/IncNumNotSelectedIcon'
+import IncNumSelectedIcon from '../Reskin/IncNumSelectedIcon'
+import InfoIconTransparent from '../Reskin/InfoIconTransparent'
+import LeftArrow from '../Reskin/LeftArrow'
+import LeftArrowLong from '../Reskin/LeftArrowLong'
+import ListNotSelectedIcon from '../Reskin/ListNotSelectedIcon'
+import ListSelectedIcon from '../Reskin/ListSelectedIcon'
+import LockIcon from '../Reskin/LockIcon'
+import LoopIcon from '../Reskin/LoopIcon'
+import MaximizeIcon from '../Reskin/MaximizeIcon'
+import MouseLeftSelected from '../Reskin/MouseLeftSelected'
+import MouseRightSelected from '../Reskin/MouseRightSelected'
+import NetworkErrorIcon from '../Reskin/NetworkErrorIcon'
+import ReloadIcon from '../Reskin/ReloadIcon'
+import RightArrowLong from '../Reskin/RightArrowLong'
+import ShareIcon from '../Reskin/ShareIcon'
+import SortDuplicatesIcon from '../Reskin/SortDuplicatesIcon'
+import SourceNotFoundIcon from '../Reskin/SourceNotFound'
+import SprayClickIcon from '../Reskin/SprayClickIcon'
+import StorageIcon from '../Reskin/StorageIcon'
+import UploadFolder from '../Reskin/UploadFolder'
+import WarningIcon from '../Reskin/WarningIcon'
+import WarningImgMovedIcon from '../Reskin/WarningImgMovedIcon'
+import ZoomIcon from '../Reskin/ZoomIcon'
+import AlertIcon from '../xx/AlertIconDD'
 import ArrowDownIcon from './ArrowDownIcon'
 import ArrowIcon from './ArrowFillIcon'
 import ArrowUpIcon from './ArrowUpIcon'
-
-import AlertIcon from '../xx/AlertIconDD'
-
 import BorderColorIcon from './BorderColorIcon'
-
 import CartIcon from './CartIcon'
 import CheckIcon from './CheckIcon'
 import ClearIcon from './ClearIcon'
@@ -13,70 +37,29 @@ import ClockIcon from './ClockIcon'
 import ContactSupportIcon from './ContactSupportIcon'
 import ContentCopyIcon from './ContentCopyIcon'
 import CopyIcon from './CopyIcon'
-
 import DeleteIcon from './DeleteIcon'
 import DescAlphaIcon from './DescAlphaIcon'
 import DescNumIcon from './DescNumIcon'
 import DiamondIcon from './DiamonIcon'
-
 import EditIcon from './EditIcon'
 import EditPhotoIcon from './EditPhotoIcon'
 import ExitAppIcon from './ExitAppIcon'
 import ExportedIcon from './ExportedIcon'
-import EyeIcon from './special/EyeIcon'
-
+import InActiveFilter from './FilterIcon'
 import FolderIcon from './FolderIcon'
 import FriendsIcon from './FriendsIcon'
-
 import GridIcon from './GridIcon'
-
-import InActiveSprayCanIcon from '../Reskin/InActiveSprayCanIcon'
-import InActiveStarIcon from '../Reskin/InActiveStarIcon'
-import IncAlphaNotSelectedIcon from '../Reskin/IncAlphaNotSelectedIcon'
-import IncAlphaSelectedIcon from '../Reskin/IncAlphaSelectedIcon'
-import IncNumNotSelectedIcon from '../Reskin/IncNumNotSelectedIcon'
-import IncNumSelectedIcon from '../Reskin/IncNumSelectedIcon'
-import InfoIconTransparent from '../Reskin/InfoIconTransparent'
-import InActiveFilter from './FilterIcon'
 import ImageNotFound from './ImageNotFound'
 import ColorIcon from './inactive/ColorIcon'
 import LoupeIcon from './inactive/LoupeIcon'
 import ShortCutIcon from './inactive/ShortCutIcon'
 import SortIcon from './inactive/SortIcon'
 import InfoIcon from './InfoIcon'
-
-import LeftArrow from '../Reskin/LeftArrow'
-import LeftArrowLong from '../Reskin/LeftArrowLong'
-import ListNotSelectedIcon from '../Reskin/ListNotSelectedIcon'
-import ListSelectedIcon from '../Reskin/ListSelectedIcon'
-import LockIcon from '../Reskin/LockIcon'
-import LoopIcon from '../Reskin/LoopIcon'
-
-import MaximizeIcon from '../Reskin/MaximizeIcon'
-import MouseLeftSelected from '../Reskin/MouseLeftSelected'
-import MouseRightSelected from '../Reskin/MouseRightSelected'
 import MenuIcon from './MenuIcon'
-
-import NetworkErrorIcon from '../Reskin/NetworkErrorIcon'
-
 import ProcessIcon from './ProcessIcon'
-
-import ReloadIcon from '../Reskin/ReloadIcon'
-import RightArrowLong from '../Reskin/RightArrowLong'
-
-import ShareIcon from '../Reskin/ShareIcon'
-import SortDuplicatesIcon from '../Reskin/SortDuplicatesIcon'
-import SourceNotFoundIcon from '../Reskin/SourceNotFound'
-import SprayClickIcon from '../Reskin/SprayClickIcon'
-import StorageIcon from '../Reskin/StorageIcon'
+import EyeIcon from './special/EyeIcon'
+import SprayCanIcon from './SprayCanIcon'
 import StarRateIcon from './StarRateIcon'
-
-import UploadFolder from '../Reskin/UploadFolder'
-
-import WarningIcon from '../Reskin/WarningIcon'
-import WarningImgMovedIcon from '../Reskin/WarningImgMovedIcon'
-
-import ZoomIcon from '../Reskin/ZoomIcon'
 
 export {
 	AlertIcon,
@@ -108,7 +91,7 @@ export {
 	InActiveFilter,
 	ShortCutIcon,
 	SortIcon,
-	InActiveSprayCanIcon,
+	SprayCanIcon,
 	InActiveStarIcon,
 	LoupeIcon,
 	IncAlphaNotSelectedIcon,
