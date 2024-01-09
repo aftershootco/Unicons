1da629fe2be5b7a1b371e69678f20f30f2a74a7f
booi-dev <booimangang@gmail.com>
2024-01-09 17:32:31 +0530

    add: reload icon

---
+++ src/stories/ReloadIcon.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
+
+import { ReloadIcon } from '../Icons'
+
+const meta: Meta<typeof ReloadIcon> = {
+	component: ReloadIcon,
+	title: 'Icons/ReloadIcon',
+	decorators: [
+		(Story) => (
+			<div style={{ margin: '1rem' }}>
+				<Story />
+			</div>
+		),
+	],
+}
+
+export default meta
+type Story = StoryObj<typeof ReloadIcon>
+
+export const Default: Story = {}
+
+export const Red: Story = {
+	args: {
+		color: 'red',
+	},
+}
+
+export const Large: Story = {
+	args: {
+		size: 30,
+	},
+}
